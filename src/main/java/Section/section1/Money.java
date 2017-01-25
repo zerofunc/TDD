f95e5974258e06e9aeaaade902882c74d5fe9d45
HyeongJu Yoo <zerofunc@naver.com>
2017-01-25 23:35:30 +0900

    8장 Dollar Franc 중복

--- src/main/java/Section/section1/Money.java
+++ src/main/java/Section/section1/Money.java
@@ -3,12 +3,22 @@ package Section.section1;
 /**
  * Created by YooHyeongJu on 2017-01-25.
  */
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    public abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object o) {
         Money money = (Money) o;
         return amount == money.amount && getClass().equals(money.getClass());
+    }
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
     }
 }
--- src/test/java/Section/s1.java
+++ src/test/java/Section/s1.java
@@ -2,6 +2,7 @@ package Section;
 
 import Section.section1.Dollar;
 import Section.section1.Franc;
+import Section.section1.Money;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertFalse;
@@ -14,17 +15,17 @@ import static org.junit.Assert.assertTrue;
 public class s1 {
     @Test
     public void testMultiplication() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
     public void testEquality() {
-        assertTrue((new Dollar(5).equals((new Dollar(5)))));
-        assertFalse((new Dollar(5).equals((new Dollar(6)))));
+        assertTrue((Money.dollar(5).equals((Money.dollar(5)))));
+        assertFalse((Money.dollar(5).equals((Money.dollar(6)))));
         assertTrue((new Franc(5).equals((new Franc(5)))));
-        assertFalse((new Franc(5).equals((new Dollar(5)))));
+        assertFalse((new Franc(5).equals((Money.dollar(5)))));
     }
 
     @Test
